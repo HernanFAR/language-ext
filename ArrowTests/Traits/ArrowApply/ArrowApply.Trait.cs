58240a9bfa6066b14e4c1bfef55d6e6a57b86811
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-09 17:20:15 -0300

    Transfer ArrowApply trait prototype

---
+++ ArrowTests/Traits/ArrowApply/ArrowApply.Trait.cs
@@ -0,0 +1,19 @@
+﻿using LanguageExt.Traits;
+
+namespace ArrowTests.Traits;
+
+public interface ArrowApply<F> : Arrow<F>
+    where F : ArrowApply<F>
+{
+    static abstract K<F, (K<F, I, O> Arrow, I Input), O> Apply<I, O>();
+
+    static virtual K<F, I, O> Bind<I, A, O>(
+        K<F, I, A> first,
+        Func<A, K<F, A, O>> next) =>
+        F.Compose(
+            first,
+            F.Compose(
+                F.Lift<A, (K<F, A, O> Arrow, A Input)>(
+                    value => (next(value), value)),
+                F.Apply<A, O>()));
+}
