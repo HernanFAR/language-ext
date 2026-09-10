073c0328a8d03178dc7da8772c33e714e543c424
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-09 22:18:52 -0300

    Align Category module naming with LanguageExt conventions

--- LanguageExt.Core/Traits/Category/Category.Module.cs
+++ LanguageExt.Core/Traits/Category/Category.Module.cs
@@ -4,48 +4,47 @@ namespace LanguageExt.Traits;
 
 public static class Category
 {
-    public static K<F, A, A> Identity<F, A>()
+    public static K<F, A, A> identity<F, A>()
         where F : Category<F> =>
         F.Identity<A>();
 
-    public static K<F, I1, O> Compose<F, I1, I2, O>(
-        K<F, I1, I2> first,
-        K<F, I2, O> second)
+    public static K<F, A, C> compose<F, A, B, C>(
+        K<F, A, B> first,
+        K<F, B, C> second)
         where F : Category<F> =>
         F.Compose(first, second);
 
-    public static K<F, I1, O> Compose<F, I1, I2, I3, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, O> third)
+    public static K<F, A, D> compose<F, A, B, C, D>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third)
         where F : Category<F> =>
         F.Compose(first, second, third);
 
-    public static K<F, I1, O> Compose<F, I1, I2, I3, I4, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, I4> third,
-        K<F, I4, O> fourth)
+    public static K<F, A, E> compose<F, A, B, C, D, E>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third,
+        K<F, D, E> fourth)
         where F : Category<F> =>
         F.Compose(first, second, third, fourth);
 
-    public static K<F, I1, O> Compose<F, I1, I2, I3, I4, I5, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, I4> third,
-        K<F, I4, I5> fourth,
-        K<F, I5, O> fifth)
+    public static K<F, A, G> compose<F, A, B, C, D, E, G>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third,
+        K<F, D, E> fourth,
+        K<F, E, G> fifth)
         where F : Category<F> =>
         F.Compose(first, second, third, fourth, fifth);
 
-    public static K<F, I1, O> Compose<F, I1, I2, I3, I4, I5, I6, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, I4> third,
-        K<F, I4, I5> fourth,
-        K<F, I5, I6> fifth,
-        K<F, I6, O> sixth)
+    public static K<F, A, H> compose<F, A, B, C, D, E, G, H>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third,
+        K<F, D, E> fourth,
+        K<F, E, G> fifth,
+        K<F, G, H> sixth)
         where F : Category<F> =>
         F.Compose(first, second, third, fourth, fifth, sixth);
-
 }
