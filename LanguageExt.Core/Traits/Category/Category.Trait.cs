14d03ad3e33cfb05f34ef779f3a76fb64b982d6c
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-09 22:18:45 -0300

    Normalize Category type parameter naming

--- LanguageExt.Core/Traits/Category/Category.Trait.cs
+++ LanguageExt.Core/Traits/Category/Category.Trait.cs
@@ -7,37 +7,37 @@ public interface Category<F>
 {
     public static abstract K<F, A, A> Identity<A>();
 
-    public static abstract K<F, I1, O> Compose<I1, I2, O>(
-        K<F, I1, I2> first,
-        K<F, I2, O> second);
+    public static abstract K<F, A, C> Compose<A, B, C>(
+        K<F, A, B> first,
+        K<F, B, C> second);
 
-    public static virtual K<F, I1, O> Compose<I1, I2, I3, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, O> third) =>
+    public static virtual K<F, A, D> Compose<A, B, C, D>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third) =>
         F.Compose(first, F.Compose(second, third));
 
-    public static virtual K<F, I1, O> Compose<I1, I2, I3, I4, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, I4> third,
-        K<F, I4, O> fourth) =>
+    public static virtual K<F, A, E> Compose<A, B, C, D, E>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third,
+        K<F, D, E> fourth) =>
         F.Compose(first, F.Compose(second, F.Compose(third, fourth)));
 
-    public static virtual K<F, I1, O> Compose<I1, I2, I3, I4, I5, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, I4> third,
-        K<F, I4, I5> fourth,
-        K<F, I5, O> fifth) =>
+    public static virtual K<F, A, G> Compose<A, B, C, D, E, G>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third,
+        K<F, D, E> fourth,
+        K<F, E, G> fifth) =>
         F.Compose(first, F.Compose(second, F.Compose(third, F.Compose(fourth, fifth))));
 
-    public static virtual K<F, I1, O> Compose<I1, I2, I3, I4, I5, I6, O>(
-        K<F, I1, I2> first,
-        K<F, I2, I3> second,
-        K<F, I3, I4> third,
-        K<F, I4, I5> fourth,
-        K<F, I5, I6> fifth,
-        K<F, I6, O> sixth) =>
+    public static virtual K<F, A, H> Compose<A, B, C, D, E, G, H>(
+        K<F, A, B> first,
+        K<F, B, C> second,
+        K<F, C, D> third,
+        K<F, D, E> fourth,
+        K<F, E, G> fifth,
+        K<F, G, H> sixth) =>
         F.Compose(first, F.Compose(second, F.Compose(third, F.Compose(fourth, F.Compose(fifth, sixth)))));
 }
